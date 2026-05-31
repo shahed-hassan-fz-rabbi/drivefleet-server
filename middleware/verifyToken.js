a20d5bb7c2ea76b7830382095d1a32af0b6817fe
codeinfinite19 <shahedhassan572@gmail.com>
2026-05-31 17:58:10 +0600

    jwt token issues

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,7 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
+  let token = req.cookies?.token;
+
+  if (!token) {
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    }
+  }
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
